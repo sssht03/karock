3cd2ff2c8b025d24daa2cd54e5069284f501b773
松尾周汰 <shutamatsuo@MacBook-Air-4.local>
2024-05-12 05:35:23 +0900

    implement getData

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,55 +1,89 @@
 import { initializeApp, type FirebaseApp } from "firebase/app";
 import { getAnalytics, type Analytics } from "firebase/analytics";
-import { Firestore, doc, getFirestore, setDoc } from "firebase/firestore";
-import { getStorage, ref as storageRef, uploadBytes, getDownloadURL, type FirebaseStorage } from "firebase/storage";
+import {
+  Firestore,
+  doc,
+  documentId,
+  getDoc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
 
-export default defineNuxtPlugin(nuxtApp => {
-	const config = useRuntimeConfig();
+import {
+  getStorage,
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+  type FirebaseStorage,
+} from "firebase/storage";
 
-	const firebaseConfig: any = {
-		apiKey: config.public.firebaseApiKey,
-		authDomain: config.public.firebaseAuthDomain,
-		projectId: config.public.firebaseProjectId,
-		storageBucket: config.public.firebaseStorageBucket,
-		messagingSenderId: config.public.firebaseMessagingSenderId,
-		appId: config.public.firebaseAppId,
-		measurementId: config.public.firebaseMeasurementId
-	};
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
 
-	// Initialize Firebase
-	const app: FirebaseApp = initializeApp(firebaseConfig);
-	const analytics: Analytics = getAnalytics(app);
-	const db: Firestore = getFirestore(app);
-	const storage: FirebaseStorage = getStorage(app);
+  const firebaseConfig: any = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId,
+    measurementId: config.public.firebaseMeasurementId,
+  };
 
-	const uploadData = async (file: File, giftTitle: string, fromName: string, toName: string, message: string, documentId: string): Promise<void> => {
-		console.log('uploadData')
-		try {
-			// Storageに動画をアップロード
-			const fileRef = storageRef(storage, `videos/${file.name}`);
-			const snapshot = await uploadBytes(fileRef, file);
-			console.log(snapshot)
-			const videoUrl = await getDownloadURL(snapshot.ref);
+  // Initialize Firebase
+  const app: FirebaseApp = initializeApp(firebaseConfig);
+  const analytics: Analytics = getAnalytics(app);
+  const db: Firestore = getFirestore(app);
+  const storage: FirebaseStorage = getStorage(app);
 
-			// Firestoreにデータをセット
-			const docRef = doc(db, "gifts", documentId);
-			await setDoc(docRef, {
-				videoUrl,
-				giftTitle,
-				fromName,
-				toName,
-				message
-			});
+  const uploadData = async (
+    file: File,
+    giftTitle: string,
+    fromName: string,
+    toName: string,
+    message: string,
+    documentId: string
+  ): Promise<void> => {
+    console.log("uploadData");
+    try {
+      // Storageに動画をアップロード
+      const fileRef = storageRef(storage, `videos/${file.name}`);
+      const snapshot = await uploadBytes(fileRef, file);
+      console.log(snapshot);
+      const videoUrl = await getDownloadURL(snapshot.ref);
 
-			console.log("データが正常にアップロードされました");
-		} catch (error) {
-			console.error("アップロード中にエラーが発生しました: ", error);
-		}
-	}
+      // Firestoreにデータをセット
+      const docRef = doc(db, "gifts", documentId);
+      await setDoc(docRef, {
+        videoUrl,
+        giftTitle,
+        fromName,
+        toName,
+        message,
+      });
+      alert("データが正常にアップロードされました");
+    } catch (error) {
+      alert("アップロード中にエラーが発生しました: ");
+      console.log("ERROR: ", error);
+    }
+  };
 
-	return {
-		provide: {
-			uploadData
-		}
-	}
-})
+  const getStoreData = async (documentId: string) => {
+    const docRef = doc(db, "gifts", documentId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      alert("データが存在しません");
+      return null;
+    }
+  };
+
+  return {
+    provide: {
+      uploadData,
+      getStoreData,
+    },
+  };
+});
