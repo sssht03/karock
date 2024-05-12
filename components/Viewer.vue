2808db7f665520208555357cf794da57cd21317b
松尾周汰 <shutamatsuo@MacBook-Air-4.local>
2024-05-13 00:32:16 +0900

    fix video

--- components/Viewer.vue
+++ components/Viewer.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="viewer">
     <h1>Viewer</h1>
-    <video :src="giftStore.videoUrl" autoplay loop></video>
+    <video :src="giftStore.videoUrl" playsinline controls></video>
     <p>{{ giftStore.title }}</p>
     <p>{{ giftStore.fromName }}</p>
     <p>{{ giftStore.toName }}</p>
